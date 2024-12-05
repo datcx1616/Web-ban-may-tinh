80959613131712a0faf1d2f5da745905242d7fba
datcx1616 <datcx123@gmail.com>
2024-12-05 09:55:44 +0700

    fixxx

--- resources/views/client/shop/index.blade.php
+++ resources/views/client/shop/index.blade.php
@@ -80,54 +80,67 @@
 
 
 
-    <div class="container-fluid">
-        <div class="row px-xl-5 slide-container">
+    <div class="container-fluid pt-3">
+        <div class="row px-xl-5 pb-1 slide-container">
             <div class="col-lg-2 col-md-6 col-sm-12 pb-1 slide-item">
                 <div class="d-flex align-items-center bg-light mb-4"
-                    style="padding: 30px; border-radius: 10px; height: 100px;">
-                    <img src="{{ asset('client/img/icon1.png') }}" alt="logo" style="width: 50px; height: 40px;">
-                    <h5 class="font-weight-semi-bold m-0 mx-3">App</h5>
-                    <img src="{{ asset('client/img/icon1.1.png') }}" alt="logo" style="width: 100px; height: 70px;">
+                    style="padding: 20px; border-radius: 10px; height: 100px;">
+                    <img src="{{ asset('client/img/icon1.png') }}" alt="logo"
+                        style="width: 50px; height: 40px; margin-right: 10px;">
+                    <h5 class="font-weight-semi-bold m-0 text-nowrap" style="margin-left: 0;">Apple</h5>
+                    <img src="{{ asset('client/img/icon1.1.png') }}" alt="logo"
+                        style="width: 100px; height: 70px; margin-left: auto;">
+                </div>
+            </div>
+
+            <div class="col-lg-2 col-md-6 col-sm-12 pb-1 slide-item">
+                <div class="d-flex align-items-center bg-light mb-4"
+                    style="padding: 20px; border-radius: 10px; height: 100px;">
+                    <img src="{{ asset('client/img/icon7.png') }}" alt="logo"
+                        style="width: 50px; height: 40px; margin-right: 10px;">
+                    <h5 class="font-weight-semi-bold m-0 text-nowrap" style="margin-left: 0;">hp</h5>
+                    <img src="{{ asset('client/img/icon7.7.png') }}" alt="logo"
+                        style="width: 100px; height: 70px; margin-left: auto;">
                 </div>
             </div>
             <div class="col-lg-2 col-md-6 col-sm-12 pb-1 slide-item">
                 <div class="d-flex align-items-center bg-light mb-4"
-                    style="padding: 30px; border-radius: 10px; height: 100px;">
-                    <img src="{{ asset('client/img/icon7.png') }}" alt="logo" style="width: 50px; height: 40px;">
-                    <h5 class="font-weight-semi-bold m-0 mx-3">hp</h5>
-                    <img src="{{ asset('client/img/icon7.7.png') }}" alt="logo" style="width: 100px; height: 70px;">
+                    style="padding: 20px; border-radius: 10px; height: 100px;">
+                    <img src="{{ asset('client/img/icon3.png') }}" alt="logo"
+                        style="width: 50px; height: 40px; margin-right: 10px;">
+                    <h5 class="font-weight-semi-bold m-0 text-nowrap" style="margin-left: 0;">Acer</h5>
+                    <img src="{{ asset('client/img/icon3.3.png') }}" alt="logo"
+                        style="width: 100px; height: 70px; margin-left: auto;">
                 </div>
             </div>
             <div class="col-lg-2 col-md-6 col-sm-12 pb-1 slide-item">
                 <div class="d-flex align-items-center bg-light mb-4"
-                    style="padding: 30px; border-radius: 10px; height: 100px;">
-                    <img src="{{ asset('client/img/icon3.png') }}" alt="logo" style="width: 50px; height: 40px;">
-                    <h5 class="font-weight-semi-bold m-0 mx-3">Acer</h5>
-                    <img src="{{ asset('client/img/icon3.3.png') }}" alt="logo" style="width: 100px; height: 70px;">
+                    style="padding: 20px; border-radius: 10px; height: 100px;">
+                    <img src="{{ asset('client/img/icon4.png') }}" alt="logo"
+                        style="width: 50px; height: 40px; margin-right: 10px;">
+                    <h5 class="font-weight-semi-bold m-0 text-nowrap" style="margin-left: 0;">MIS</h5>
+                    <img src="{{ asset('client/img/icon4.4.png') }}" alt="logo"
+                        style="width: 100px; height: 70px; margin-left: auto;">
                 </div>
             </div>
             <div class="col-lg-2 col-md-6 col-sm-12 pb-1 slide-item">
                 <div class="d-flex align-items-center bg-light mb-4"
-                    style="padding: 30px; border-radius: 10px; height: 100px;">
-                    <img src="{{ asset('client/img/icon4.png') }}" alt="logo" style="width: 50px; height: 40px;">
-                    <h5 class="font-weight-semi-bold m-0 mx-3">MSI</h5>
-                    <img src="{{ asset('client/img/icon4.4.png') }}" alt="logo" style="width: 100px; height: 70px;">
+                    style="padding: 20px; border-radius: 10px; height: 100px;">
+                    <img src="{{ asset('client/img/icon5.png') }}" alt="logo"
+                        style="width: 50px; height: 40px; margin-right: 10px;">
+                    <h5 class="font-weight-semi-bold m-0 text-nowrap" style="margin-left: 0;">Lenovo</h5>
+                    <img src="{{ asset('client/img/icon5.5.png') }}" alt="logo"
+                        style="width: 100px; height: 70px; margin-left: auto;">
                 </div>
             </div>
             <div class="col-lg-2 col-md-6 col-sm-12 pb-1 slide-item">
                 <div class="d-flex align-items-center bg-light mb-4"
-                    style="padding: 30px; border-radius: 10px; height: 100px;">
-                    <img src="{{ asset('client/img/icon5.png') }}" alt="logo" style="width: 50px; height: 40px;">
-                    <h5 class="font-weight-semi-bold m-0 mx-3">Leno</h5>
-                    <img src="{{ asset('client/img/icon5.5.png') }}" alt="logo" style="width: 100px; height: 70px;">
-                </div>
-            </div>
-            <div class="col-lg-2 col-md-6 col-sm-12 pb-1 slide-item">
-                <div class="d-flex align-items-center bg-light mb-4"
-                    style="padding: 30px; border-radius: 10px; height: 100px;">
-                    <img src="{{ asset('client/img/icon6.png') }}" alt="logo" style="width: 50px; height: 40px;">
-                    <h5 class="font-weight-semi-bold m-0 mx-3">Dell</h5>
-                    <img src="{{ asset('client/img/icon6.6.png') }}" alt="logo" style="width: 100px; height: 70px;">
+                    style="padding: 20px; border-radius: 10px; height: 100px;">
+                    <img src="{{ asset('client/img/icon6.png') }}" alt="logo"
+                        style="width: 50px; height: 40px; margin-right: 10px;">
+                    <h5 class="font-weight-semi-bold m-0 text-nowrap" style="margin-left: 0;">Dell</h5>
+                    <img src="{{ asset('client/img/icon6.6.png') }}" alt="logo"
+                        style="width: 100px; height: 70px; margin-left: auto;">
                 </div>
             </div>
         </div>
