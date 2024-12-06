7b94fb0122f39fb193e333b99f77b9252da7f43d
datcx1616 <datcx123@gmail.com>
2024-12-06 13:18:01 +0700

    dww

--- resources/views/client/contact/index.blade.php
+++ resources/views/client/contact/index.blade.php
@@ -47,8 +47,10 @@
                 <div class="bg-light p-30 mb-30" style=" border-radius: 10px;">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3780.1102349383673!2d105.69317477432313!3d18.659048664931277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3139cddf0bf20f23%3A0x86154b56a284fa6d!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBWaW5o!5e0!3m2!1svi!2s!4v1699982238510!5m2!1svi!2s"
-                        width="430" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        width="680" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade">
+                    </iframe>
+
                 </div>
                 <div class="bg-light p-30 mb-3" style=" border-radius: 10px;">
                     <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>182 Lê Duẩn, Thành phố Vinh</p>
