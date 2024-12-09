7ce85b37ef7b635ebdd0ae78b22a565269afacef
Cao Xuan Dat <datcx123@gmail.com>
2024-12-09 13:38:08 +0700

    fixxx

--- public/client/js/snow.js
+++ public/client/js/snow.js
@@ -3,17 +3,17 @@ const snowContainer = document.querySelector(".snow-container");
 function createSnowflake() {
     const snowflake = document.createElement("img"); // Tạo thẻ img
     snowflake.classList.add("snowflake");
-    snowflake.src = "/client/img/hoadao1.png"; // Đường dẫn tới ảnh bông tuyết
+    snowflake.src = "/client/img/hoadao1.png"; // Đường dẫn tới ảnh hoa đào
     snowflake.style.left = Math.random() * 100 + "vw"; // Vị trí ngang ngẫu nhiên
-    snowflake.style.animationDuration = Math.random() * 12 + 10 + "s"; // Thời gian rơi (7-12 giây)
+    snowflake.style.animationDuration = Math.random() * 20 + 15 + "s"; // Thời gian rơi (15-35 giây)
     snowflake.style.width = Math.random() * 30 + 15 + "px"; // Kích thước ngẫu nhiên
 
     snowContainer.appendChild(snowflake);
 
-    // Xóa bông tuyết sau khi rơi xong
+    // Xóa hoa đào sau khi rơi xong
     setTimeout(() => {
         snowflake.remove();
-    }, 12000); // Xóa sau 12 giây
+    }, 35000); // Xóa sau 35 giây (tương ứng với animationDuration lớn nhất)
 }
 
-setInterval(createSnowflake, 500); // Tạo bông tuyết mỗi 50ms
+setInterval(createSnowflake, 500); // Tạo hoa đào mỗi 500ms
