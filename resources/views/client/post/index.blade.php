7e0f774bf23377e706bd4cb38dabe47c5cbaac4a
datcx1616 <datcx123@gmail.com>
2024-12-16 10:56:03 +0700

    fixxxxxx

--- resources/views/client/post/index.blade.php
+++ resources/views/client/post/index.blade.php
@@ -79,7 +79,7 @@
             @endforeach
         </div>
     </div>
-    <div class="container-fluid pt-5 pb-3">
+    <div class="container-fluid pt-3 pb-3">
         <h2 class="section-title position-relative text-uppercase mx-xl-5 mb-4">
             <span class="bg-secondary pr-3">Video Hot</span>
         </h2>
